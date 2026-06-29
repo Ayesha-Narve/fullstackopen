ef52fe15de2ceb9c734555d391ff6a55d50f306d
Ayesha Narve <narveayesha@gmail.com>
2026-06-29 23:50:14 +0530

    exercise 1.9 completed

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
 const Statistics = (props) => {
+  if (props.all === 0) {
+    return <p>No feedback given</p>
+  }
+
   return (
     <div>
       <h2>Statistics</h2>
