2431a2031412e5492ff42c1cfb355a39e4d6b6d9
Ayesha Narve <narveayesha@gmail.com>
2026-07-03 23:29:30 +0530

    exercise 3.11 completed

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 
 const app = express()
@@ -8,6 +9,7 @@ morgan.token('body', (request) => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
