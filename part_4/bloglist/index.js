6006a6b0e7c462225e1858cfa365a43333487ef0
Ayesha Narve <narveayesha@gmail.com>
2026-07-04 20:28:33 +0530

    exercise 4.2 completed

--- part_4/bloglist/index.js
+++ part_4/bloglist/index.js
@@ -1,39 +1,9 @@
-const express = require('express')
 const mongoose = require('mongoose')
+const app = require('./app')
 
-const app = express()
-
-const mongoUrl = 'mongodb+srv://narveayesha_db_user:n4W2Uk7dqe20WzNJ@cluster0.kijryrs.mongodb.net/?appName=Cluster0'
+const mongoUrl = 'YOUR_MONGODB_CONNECTION_STRING'
 
 mongoose.connect(mongoUrl)
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 const PORT = 3003
 
