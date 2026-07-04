76d2bf9732c8ce6e27f542bce82962b36082091c
Ayesha Narve <narveayesha@gmail.com>
2026-07-04 21:56:20 +0530

    exercise 4.18 completed

--- part_4/bloglist/app.js
+++ part_4/bloglist/app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const Blog = require('./models/blog')
 const usersRouter = require('./controllers/users')
 const User = require('./models/user')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
 
 const app = express()
 
@@ -69,4 +72,36 @@ app.put('/api/blogs/:id', async (request, response) => {
   response.json(updatedBlog)
 })
 
+app.post('/api/login', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.findOne({ username })
+
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user.id
+  }
+
+  const token = jwt.sign(
+    userForToken,
+    'SECRET'
+  )
+
+  response.status(200).send({
+    token,
+    username: user.username,
+    name: user.name
+  })
+})
 module.exports = app
