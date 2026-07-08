77fb75388bf3c7fb90b03674c8deb0e47ff9f70e
Ayesha Narve <narveayesha@gmail.com>
2026-07-09 01:16:37 +0530

    exercise 5.15 completed

--- part_5/bloglist-frontend/src/components/Blog.test.jsx
+++ part_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
 import Blog from './Blog'
 
 test('renders title and author but not url or likes by default', () => {
@@ -57,4 +58,40 @@ test('shows url and likes when view button is clicked', async () => {
 
   expect(screen.getByText('https://react.dev')).toBeInTheDocument()
   expect(screen.getByText(/likes 10/i)).toBeInTheDocument()
+})
+
+test('clicking the like button twice calls event handler twice', async () => {
+  const blog = {
+    title: 'React Testing',
+    author: 'Ayesha',
+    url: 'https://react.dev',
+    likes: 10,
+    user: {
+      username: 'ayesha',
+      name: 'Ayesha'
+    }
+  }
+
+  const mockHandler = vi.fn()
+
+  const user = userEvent.setup()
+
+  render(
+    <Blog
+      blog={blog}
+      updateLikes={mockHandler}
+      deleteBlog={() => {}}
+      user={blog.user}
+    />
+  )
+
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const likeButton = screen.getByText('like')
+
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
