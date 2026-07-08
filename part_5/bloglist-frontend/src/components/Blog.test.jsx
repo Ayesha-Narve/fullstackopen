dc8d2dc695ad09b1c7db09fc76b00a2a3fe614b7
Ayesha Narve <narveayesha@gmail.com>
2026-07-09 01:13:22 +0530

    exercise 5.14 completed

--- part_5/bloglist-frontend/src/components/Blog.test.jsx
+++ part_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 import Blog from './Blog'
 
@@ -24,8 +25,36 @@ test('renders title and author but not url or likes by default', () => {
   )
 
   expect(screen.getByText('React Testing Ayesha')).toBeInTheDocument()
-
   expect(screen.queryByText('https://react.dev')).toBeNull()
-
   expect(screen.queryByText(/likes/i)).toBeNull()
+})
+
+test('shows url and likes when view button is clicked', async () => {
+  const blog = {
+    title: 'React Testing',
+    author: 'Ayesha',
+    url: 'https://react.dev',
+    likes: 10,
+    user: {
+      username: 'ayesha',
+      name: 'Ayesha'
+    }
+  }
+
+  const user = userEvent.setup()
+
+  render(
+    <Blog
+      blog={blog}
+      updateLikes={() => {}}
+      deleteBlog={() => {}}
+      user={blog.user}
+    />
+  )
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(screen.getByText('https://react.dev')).toBeInTheDocument()
+  expect(screen.getByText(/likes 10/i)).toBeInTheDocument()
 })
